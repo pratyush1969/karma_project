e789715147267a69db522a1cabc44ba00831bf25
Pratyush Sarkar <pratyushsarkar41@gmail.com>
2023-10-07 17:56:03 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Contact from './Contact';
 // import Fun from './fun';
 import Dice from './dice';
 import Signin from "./signin";
+import Fun from './Fun'
 function App() {
   return (
     <Router>
@@ -31,7 +32,8 @@ function App() {
             <Route path="/frog"element ={<FrogGame/>}/>
             <Route path="/contact" element={<Contact/>}/>
 
-            
+            <Route path="/fun" element={<Fun/>}/>
+
             <Route path="/dice"element ={<Dice/>}/>
           </Routes>
      </div>
---
+++ src/Fun.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+const Fun = () => {
+  return (
+    <div>
+      <div className='others'>
+        <Link to="https://codecombat.com/"><img src="https://tse1.mm.bing.net/th?id=OIP.byE5LjkvICmew8cLHJMIvAHaEo&pid=Api&P=0&h=180" alt="" /></Link>
+        <Link to="https://flexboxfroggy.com/"><img src="https://flexboxfroggy.com/images/screenshot.png" alt="" /></Link>
+        <Link to="https://flukeout.github.io/"><img src="https://tse2.mm.bing.net/th?id=OIP.9990_teJIcwIu1ZKgCC_bgAAAA&pid=Api&P=0&h=180" alt="" /></Link>
+        <Link to="https://www.abcya.com/"><img src="https://tse2.explicit.bing.net/th?id=OIP.mT9sJowOYjWUIgfPcMJs_wHaEK&pid=Api&P=0&h=180" alt="" /></Link>
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Fun
